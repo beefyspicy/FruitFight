384cde9772b8a8df84c07ffa965f38cc440713d1
beefyspicy <hayden.g.archambault@gmail.com>
2019-03-25 02:25:56 -0400

    added another song and put new weapons in more levels

---
+++ FruitFight/Assets/Scripts/Not2D/MusicKeepsPlaying.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicKeepsPlaying : MonoBehaviour {
+
+	void Update ()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        Scene currentScene = SceneManager.GetActiveScene();
+        string sceneName = currentScene.name;
+
+        if (sceneName == "InteractiveMenu")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
